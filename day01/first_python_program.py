28ae87e1e9110d30f5a042931acce26bac24bb99
rlylo <lylomain@gmail.com>
2025-04-07 18:23:42 -0500

    initial commit

---
+++ day01/first_python_program.py
@@ -0,0 +1,10 @@
+print("Python Programming")
+#comment
+"""
+Multi
+Line
+Comments
+"""
+
+print("Python \"Programming\"")
+print("I love 'Python'")
---
+++ day01/variables.py
@@ -0,0 +1,32 @@
+"""
+Java Variables:
+    String varName
+
+Python Variable or in dynamically typed programming languages:
+    varName = data
+"""
+
+name_of_student = "elena"
+
+print(name_of_student)
+
+name_of_student = 3.5
+
+print(name_of_student)
+
+name_of_student = True
+
+print(name_of_student)
+print(type(name_of_student))
+
+s = '35'
+num = int(s)
+print(num)
+print(type(num))
+
+
+print("-----------------------------------------------------------------")
+programing_language = "Java"
+version = 3.13
+print("My programming language is {} programming language and version is {}".format(programing_language,version))
+print(f"My programming language is {programing_language} and version {version}")
